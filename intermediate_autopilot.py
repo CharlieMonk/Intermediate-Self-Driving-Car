46030eeaa75bda0d059447890325341ec95ecd74
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-02-25 17:56:29 -0500

    Lots of stuff
    1.) Add new test images
    2.) Functionize lambd in shape detection
    3.) Deprecate findContours.

--- intermediate_autopilot.py
+++ intermediate_autopilot.py
@@ -1,49 +1,33 @@
 import numpy as np
 import cv2
 import shape_detection as detectShape
-
-def findContours(edges):
-    # Finds the contours of Canny edges and computes the solidity
-    for edge in edges:
-        img2, contour, hierarchy = cv2.findContours(edge, 1, 2)
-        contour_area = 1# cv2.contourArea(contour)
-        if(contour_area > 50):
-            hull_area = cv2.contourArea(cv2.convexHull(contour))
-            solidity = float(contour_area)/hull_area
+import time
 
 def drawBoundingRects(img, bgr_img, edges):
     # Finds the contours of the Canny edges
     img2, contours, hierarchy = cv2.findContours(edges, 1, 2)
     for contour in contours:
         contour_area = cv2.contourArea(contour)
-        if(contour_area > 20):
+        if(contour_area > 10):
             print("Contour area: " + str(contour_area))
             if(detectShape.isRect(contour)):
                 rect = cv2.minAreaRect(contour)
                 box = np.int0(cv2.boxPoints(rect))
                 cv2.drawContours(bgr_img, [box], 0, (0,0,255))
-            else:
+            diagnosticOn = False
+            if(diagnosticOn):
                 isRect, polygon = detectShape.isRectDiagnostic(contour)
                 color = (255,0,0)#*isRect + (0,0,0)*(not isRect)
                 box = cv2.polylines(bgr_img, [polygon], True, color)
-            # # Finds the portion of the contour that is convex
-            # hull = cv2.convexHull(contour)
-            # hull_area = cv2.contourArea(hull)
-            # # Finds how much of the contour is occupied by a convex shape
-            # solidity = float(contour_area)/hull_area
-            # if solidity>0.05:
-            #     rect = cv2.minAreaRect(contour)
-            #     box = np.int0(cv2.boxPoints(rect))
-            #     cv2.drawContours(img, [box], 0, (0,0,255))
-            #     print(solidity)
+                
     return img2
 
 def findRoadLines(image_path):
     bgr_img = cv2.imread(image_path, 1)
-    img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2HLS)
 
     # Lower and upper ranges for yellow center lines and white lane lines
-    center_lines_lower = np.array([210, 100, 82])
+    center_lines_lower = np.array([255, 255, 255])
     center_lines_upper = np.array([255, 255, 255]) #([160, 234, 255])
 
     # Mask image using center_lines_lower and center_lines_upper
@@ -78,10 +62,12 @@ def findRoadLines(image_path):
     cv2.imshow("edges", img2)
     return img, bgr_img
 
+time0 = time.time()
 # Run findRoadLines on a test image
-img, bgr_img = findRoadLines("/Users/cbmonk/AnacondaProjects/Advanced-Self-Driving-Car/TestImages/12.png")
+img, bgr_img = findRoadLines("/Users/cbmonk/AnacondaProjects/Advanced-Self-Driving-Car/TestImages/10.png")
 cv2.imshow("HSV", img)
 cv2.imshow("BGR", bgr_img)
+print("Total time:", time.time()-time0)
 
 # Close everything out when any key is pressed
 cv2.waitKey(0)
--- shape_detection.py
+++ shape_detection.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 
 rectSideCheck = lambda polygon: len(polygon)<6
+lambd = lambda perimeter: perimeter*0.02
 def isRect(edge):
     perimeter = cv2.arcLength(edge, True)
-    polygon = cv2.approxPolyDP(edge, perimeter*0.04, True)
+    polygon = cv2.approxPolyDP(edge, lambd(perimeter), True)
     return rectSideCheck(polygon)
 
 def isRectDiagnostic(edge):
     perimeter = cv2.arcLength(edge, True)
-    polygon = cv2.approxPolyDP(edge, perimeter*0.04, True)
+    polygon = cv2.approxPolyDP(edge, lambd(perimeter), True)
     isRect = rectSideCheck(polygon)
     return isRect, polygon
