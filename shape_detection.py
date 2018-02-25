567e33f2be1cb04661dc9a4b5d0ae6931340e6a2
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-02-25 14:38:15 -0500

    Functionized poly=rect condition

--- shape_detection.py
+++ shape_detection.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
 
+rectSideCheck = lambda polygon: len(polygon)<6
 def isRect(edge):
     perimeter = cv2.arcLength(edge, True)
     polygon = cv2.approxPolyDP(edge, perimeter*0.04, True)
-    return len(polygon)>3 and len(polygon)<6
+    return rectSideCheck(polygon)
 
 def isRectDiagnostic(edge):
     perimeter = cv2.arcLength(edge, True)
     polygon = cv2.approxPolyDP(edge, perimeter*0.04, True)
-    isRect = len(polygon)>3 and len(polygon)<6
+    isRect = rectSideCheck(polygon)
     return isRect, polygon
