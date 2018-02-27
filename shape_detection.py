03d5902f1c194d057902508bee9f25e32455c316
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-02-27 12:17:27 -0500

    Turn shape_detection into a class

--- shape_detection.py
+++ shape_detection.py
@@ -1,15 +1,25 @@
 import numpy as np
 import cv2
 
-rectSideCheck = lambda polygon: len(polygon)<6
-lambd = lambda perimeter: perimeter*0.02
-def isRect(edge):
-    perimeter = cv2.arcLength(edge, True)
-    polygon = cv2.approxPolyDP(edge, lambd(perimeter), True)
-    return rectSideCheck(polygon)
-
-def isRectDiagnostic(edge):
-    perimeter = cv2.arcLength(edge, True)
-    polygon = cv2.approxPolyDP(edge, lambd(perimeter), True)
-    isRect = rectSideCheck(polygon)
-    return isRect, polygon
+class ShapeDetection:
+    def __init__(self, contour):
+        self.contour = contour
+    rectSideCheck = lambda polygon: len(polygon)<6
+    dist = lambda self, pt1, pt2: ((pt2[1]-pt1[1])**2 + (pt2[0]-pt1[0])**2)**0.5
+    slope = lambda self, pt1, pt2: (pt2[1]-pt1[1])/(pt2[0]-pt1[0])
+    def findRect(self):
+        rect = cv2.minAreaRect(self.contour)
+        box = np.int0(cv2.boxPoints(rect))
+        return box
+    def isRect(self):
+        perimeter = cv2.arcLength(self.contour, True)
+        polygon = cv2.approxPolyDP(self.contour, perimeter*0.02, True)
+        box = self.findRect()
+        ratio = self.dist(box[0], box[1])/self.dist(box[1], box[2])
+        slope = abs(self.slope(box[1],box[2]))
+        return (ratio>3 or 1/ratio>3) and (slope>1)
+    def isRectDiagnostic(self):
+        perimeter = cv2.arcLength(self.contour, True)
+        polygon = cv2.approxPolyDP(self.contour, perimeter*0.02, True)
+        isRect = rectSideCheck(polygon)
+        return isRect, polygon
