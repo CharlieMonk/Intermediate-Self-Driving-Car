dd20f56ba81e28e69b792cb9b4b94b5fc9f83b78
Charlie Monk <CharlieMonk@users.noreply.github.com>
2018-04-26 12:46:02 -0400

    Added some documentation.

--- shape_detection.py
+++ shape_detection.py
@@ -6,9 +6,12 @@ class ShapeDetection:
         self.contour = contour
         self.height, self.width = shape[:2]
     rectSideCheck = lambda polygon: len(polygon)<6
+    # Mini- functions on 2 points (distance between, slope of, mid- pt of)
     dist = lambda self, pt1, pt2: ((pt2[1]-pt1[1])**2 + (pt2[0]-pt1[0])**2)**0.5
     slope = lambda self, pt1, pt2: (pt2[1]-pt1[1])/(pt2[0]-pt1[0]+0.0001)
     mid = lambda self, pt1, pt2:(int((pt2[0]+pt1[0])/2), int((pt2[1]+pt1[1])/2))
+
+    # Find the rectangle of a contour and the centroid thereof
     def findRectAndPt(self):
         rect = cv2.minAreaRect(self.contour)
         box = np.int0(cv2.boxPoints(rect))
@@ -19,6 +22,7 @@ class ShapeDetection:
             if(pt[1] > 0.75*self.height):
                 pt = (-10, -10)
         return box, pt
+    # Check if a rectangle is an actual rectangle (NOT noise)
     def isRect(self):
         perimeter = cv2.arcLength(self.contour, True)
         polygon = cv2.approxPolyDP(self.contour, perimeter*0.01, True)
